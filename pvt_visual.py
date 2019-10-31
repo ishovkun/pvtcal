5e52d025e2ced7f936439c533f3a1970d84c9fcc
Igor Shovkun <igshov@gmail.com>
2019-10-30 19:56:09 -0700

    converting usat table into np array

--- pvt_visual.py
+++ pvt_visual.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-fname = "/home/ishovkun/sim/blackoil/1d/gprs.data"
+fname = "/home/ishovkun/sim/Blackoil/1d/gprs.data"
 
 import WordIterator
 import KeywordIterator
@@ -14,12 +14,32 @@ for kwd, value in kwd_it:
     if (kwd == "PVDG"):
         value = np.array([float(x) for x in value])
         value = value.reshape( int(len(value) / 3), 3 )
-        df = pd.DataFrame(value, columns=["p", "Bg", "mug"])
+        # print(value.shape)
+        # df = pd.DataFrame(value, columns=["p", "Bg", "mug"])
         # plt.plot(df["p"], df["Bg"])
         # plt.plot(df["p"], 1./df["mug"])
         # plt.show()
     elif (kwd == "PVTO"):
-        print(value)
-        # value = np.array([float(x) for x in value])
-        # value = value.reshape( int(len(value) / 3), 3 )
-        # df = pd.DataFrame(value, columns=["p", "Bg", "mug"])
+        # print(value)
+        tsat = []
+        tusat = []
+        for row in value:
+            if (len(row) == 4): # saturated
+                tsat.append([float(x) for x in row])
+                # print(row)
+            else:               # unsaturated
+                row = [float(x) for x in row]
+                tsat.append(row[:4])
+                tusat.append(row[:4])
+                for i in range(4, len(row), 4):
+                    line = [tsat[-1][0]] + row[i:i+4]
+                    tusat.append(line)
+                pass
+
+        tsat = np.array(tsat)
+        tusat = np.array(tusat)
+        df = pd.DataFrame(tsat, columns=["Rs", "p", "Bo", "muo"])
+        dfu = pd.DataFrame(tusat, columns=["Rs", "p", "Bo", "muo"])
+        plt.plot(df["p"], df["Bo"])
+        plt.plot(dfu["p"], dfu["Bo"], 'ko')
+        plt.show()
